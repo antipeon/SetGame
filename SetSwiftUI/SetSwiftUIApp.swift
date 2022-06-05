e727ca31eae68d7094a34ef0a3f93179407080b3
Samat Gaynutdinov <gainutdinov.sm@phystech.edu>
2022-06-05 16:12:10 +0300

    Initial Commit

---
+++ SetSwiftUI/SetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetSwiftUIApp.swift
+//  SetSwiftUI
+//
+//  Created by Samat Gaynutdinov on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
